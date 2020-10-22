bd51c5d0261c0bf1f01bf09a1c2fc16756581e25
egle <rappee@gmail.com>
2020-10-22 16:50:38 +0300

    Creating AgedBrie class and testing it

--- create_item.py
+++ create_item.py
@@ -2,19 +2,30 @@ from gilded_rose import Item
 
 
 class RegularItem(Item):
-    """I needed Item class to have  update_quality function, but altering with Item class is against the rules"""
+    """I needed Item class to have update_quality function, but altering with Item class is against the rules"""
     def update_quality(self):
         """The Quality of an item is never more than 50.
         The Quality of an item is never negative"""
         if 50 > self.quality > 0:
             """Once the sell by date has passed, Quality degrades twice as fast"""
             if self.sell_in <= 0:
-                self.quality = self.quality - 2
+                self.quality -= 2
             else:
-                self.quality = self.quality - 1
-        self.sell_in = self.sell_in - 1
+                self.quality -= 1
+        self.sell_in -= 1
 
 
 class ItemCreate(object):
     def create(self, name, sell_in, quality):
+        if name == "Aged Brie":
+            return AgedBrie(name, sell_in, quality)
         return RegularItem(name, sell_in, quality)
+
+
+class AgedBrie(RegularItem):
+    """'Aged Brie' actually increases in Quality the older it gets"""
+    def update_quality(self):
+        if self.quality < 50:
+            self.quality += 1
+        self.sell_in -= 1
+
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -19,5 +19,30 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual(29, items[0].quality)
 
+    def test_reg_quality_passed(self):
+        items = [create_item.create("Cheese", 0, 30)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(28, items[0].quality)
+
+    def test_brie_sell_in(self):
+        items = [create_item.create("Aged Brie", 5, 40)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(4, items[0].sell_in)
+    
+    def test_brie_quality(self):
+        items = [create_item.create("Aged Brie", 5, 40)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(41, items[0].quality)
+
+    def test_brie_quality_max(self):
+        items = [create_item.create("Aged Brie", 5, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(50, items[0].quality)
+
+
 if __name__ == '__main__':
     unittest.main()
