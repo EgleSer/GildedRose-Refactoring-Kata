b0965d7b0ab51b61ddbb851659a0464c571b6769
egle <rappee@gmail.com>
2020-10-26 18:52:14 +0200

    Parametrizing test functions

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,151 +5,41 @@ from create_item import ItemCreate
 create_item = ItemCreate()
 
 
-def test_reg_sell_in():
-    items = [create_item.create("Elixir of the Mongoose", 5, 30)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].sell_in == 4
+def item_through_days(item_params, days):
+    item = create_item.create(*item_params)
+    gilded_rose = GildedRose([item])
+    for x in range(days):
+        gilded_rose.update_quality()
+    return item
 
 
-def test_reg_quality():
-    items = [create_item.create("Elixir of the Mongoose", 5, 30)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].quality == 29
+@pytest.mark.parametrize(
+    "item,expected,num_days",
+    [
+        (("Elixir of the Mongoose", 5, 30), {"sell_in": 4, "quality": 29}, 1),
+        (("Elixir of the Mongoose", 0, 30), {"quality": 28}, 1),
+        (("Elixir of the Mongoose", 5, -10), {"quality": 0}, 1),
+        (("Aged Brie", 5, 40), {"sell_in": 4, "quality": 41}, 1),
+        (("Aged Brie", 5, 50), {"quality": 50}, 1),
+        (("Aged Brie", 5, 75), {"quality": 50}, 1),
+        (("Aged Brie", 5, -5), {"quality": 0}, 1),
+        (("Sulfuras", 10, 80), {"sell_in": "N/A", "quality": 80}, 1),
+        (("Sulfuras", 10, 20), {"quality": 80}, 1),
+        (("Sulfuras", 10, 130), {"quality": 80}, 1),
+        (("Conjured Mana Cake", 6, 15), {"sell_in": 5, "quality": 13}, 1),
+        (("Conjured Stamina Pancakes", 12, 1), {"sell_in": 11, "quality": 0}, 1),
+        (("Backstage passes to a TAFKAL80ETC concert", 18, 48), {"sell_in": 17, "quality": 49}, 1),
+        (("Backstage passes to a TAFKAL80ETC concert", 18, 95), {"quality": 50}, 1),
+        (("Backstage passes to a TAFKAL80ETC concert", 10, 30), {"sell_in": 9, "quality": 32}, 1),
+        (("Backstage passes to a TAFKAL80ETC concert", 5, 30), {"sell_in": 4, "quality": 33}, 1),
+        (("Backstage passes to a TAFKAL80ETC concert", -1, 30), {"quality": 0}, 1),
 
-
-def test_reg_quality_passed():
-    items = [create_item.create("Elixir of the Mongoose", 0, 30)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].quality == 28
-
-
-def test_reg_quality_low():
-    items = [create_item.create("Elixir of the Mongoose", 5, -10)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].quality ==  0
-
-
-def test_brie_sell_in():
-    items = [create_item.create("Aged Brie", 5, 40)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].sell_in == 4
-   
-
-def test_brie_quality():
-    items = [create_item.create("Aged Brie", 5, 40)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].quality == 41
-
-
-def test_brie_quality_max():
-    items = [create_item.create("Aged Brie", 5, 50)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].quality == 50
-
-
-def test_brie_quality_over():
-    items = [create_item.create("Aged Brie", 5, 75)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].quality == 50
-
-
-def test_brie_quality_low():
-    items = [create_item.create("Aged Brie", 5, -5)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].quality == 0
-
-
-def test_sulfuras_sell_in():
-    items = [create_item.create("Sulfuras", 10, 80)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].sell_in == "N/A"
-
-
-def test_sulfuras_quality():
-    items = [create_item.create("Sulfuras", 10, 80)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].quality == 80
-
-
-def test_sulfuras_quality_low():
-    items = [create_item.create("Sulfuras", 10, 20)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].quality == 80
-
-
-def test_sulfuras_quality_over():
-    items = [create_item.create("Sulfuras", 10, 130)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].quality == 80
-
-
-def test_conjured_quality():
-    items = [create_item.create("Conjured Mana Cake", 6, 15)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].quality == 13
-
-
-def test_conjured_quality_low():
-    items = [create_item.create("Conjured Stamina Pancakes", 12, 1)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].quality == 0
-
-
-def test_backstage_quality():
-    items = [create_item.create("Backstage passes to a TAFKAL80ETC concert", 18, 48)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].quality == 49
-
-
-def test_backstage_quality_over():
-    items = [create_item.create("Backstage passes to a TAFKAL80ETC concert", 18, 95)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].quality == 50
-
-
-def test_backstage_soon():
-    items = [create_item.create("Backstage passes to a TAFKAL80ETC concert", 10, 30)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].quality == 32
-
-
-def test_backstage_very_soon():
-    items = [create_item.create("Backstage passes to a TAFKAL80ETC concert", 5, 30)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].quality == 33
-
-
-def test_backstage_expired():
-    items = [create_item.create("Backstage passes to a TAFKAL80ETC concert", -1, 30)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].quality == 0
-
-
-def test_backstage_sell_in():
-    items = [create_item.create("Backstage passes to a TAFKAL80ETC concert", 5, 30)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].sell_in == 4
+    ],
+)
+def test_item_one_day_passed(item, expected, num_days):
+    item_after_days = item_through_days(item, num_days)
+    for key in expected:
+        assert getattr(item_after_days, key) == expected[key]
 
 
 if __name__ == '__main__':
